a35dd5b972a370d00aadf9a53d6d7dbfe08198d9
Janne Ala-Aijala <janne@underdude.org>
2012-05-13 22:16:49 +0300

    Scripts and template for showing tips.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
-
+  $('#slides').cycle({ 
+    random:  1,
+    slideResize: 0,
+    containerResize: 0,
+    timeout: 0,
+    next: $('#getnext .bigbutton'),
+    before: function(currSlideElement, nextSlideElement, options, forwardFlag) {
+      $('#slides').animate({'height': $(nextSlideElement).css('height')}, 400);
+    }
+  });
   
 });
